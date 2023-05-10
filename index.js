8e34dcbe84fe6169e610f924cc2da4b7ff7df419
marianorigo18 <marianowagnerrigo@gmail.com>
2023-05-10 18:45:23 -0300

    45:15

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import {promises as fs} from "fs"
 class ProductManager{
     constructor(){
         this.path = "./productos.txt"
+        this.products = []
     }
 
     static id = 0
 
     addProducts = async (title, description, price, thumbnail, stock, code) => {
         ProductManager.id++
-        const products = {
+        const product = {
             title,
             description,
             price,
@@ -18,12 +19,38 @@ class ProductManager{
             code,
             id: ProductManager.id
         }
-        console.log(products)
-        await fs.writeFile(this.path, "Hello from txt")
+        this.products.push(product)
+        await fs.writeFile(this.path, JSON.stringify(this.products, null, "\t"))
+    }
+
+    readPorducts = async () => {
+        const response = await fs.readFile(this.path, "utf-8")
+        return JSON.parse(response)
+    }
+
+    getProducts = async () => {
+        let response = await this.readPorducts()
+        return response
+    }
+
+    getProductByCode = async (code) => {
+        let response = await this.readPorducts()
+        const result = response.find(prod => prod.code == code)
+        if(!result) return "producto no encontrado"
+        return result
     }
 
 }
 
 const products = new ProductManager()
 
-products.addProducts("product1", "kakak", 1500, "imagen1.jpg", 15, 45456)
+
+const env = async () => {
+    // products.addProducts("product1", "kakak", 1500, "imagen1.jpg", 15, 45456)
+    // products.addProducts("product2", "kakak", 1500, "imagen2.jpg", 15, 45457)
+    // products.addProducts("product3", "kakak", 1500, "imagen2.jpg", 15, 45458)
+    // products.addProducts("product3", "kakak", 1500, "imagen2.jpg", 15, 45459)
+    console.log(await products.getProductByCode(45458))
+}
+
+env()
