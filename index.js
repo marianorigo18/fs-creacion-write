d50909dbaffabba0293823c9f0becc509d6c9238
marianorigo18 <marianowagnerrigo@gmail.com>
2023-05-10 23:30:58 -0300

    fix commit

--- index.js
+++ index.js
@@ -40,6 +40,26 @@ class ProductManager{
         return result
     }
 
+    deleteProdcutcByCode = async (code) => {
+        let response = await this.readPorducts()
+        const result = response.filter(prod => prod.code !== code)
+        await fs.writeFile(this.path, JSON.stringify(result, null, "\t"))
+    }
+
+    deleteProdcutcById = async (id) => {
+        let response = await this.readPorducts()
+        const result = response.filter(prod => prod.id !== id)
+        await fs.writeFile(this.path, JSON.stringify(result, null, "\t"))
+    }
+
+    updateProduct = async ({id, ...product}) => {
+        await this.deleteProdcutcById(id)
+        let products = await this.readPorducts()
+        let newProducts = [{...product, id}, ...products]
+        await fs.writeFile(this.path, JSON.stringify(newProducts, null, "\t"))
+
+    }
+
 }
 
 const products = new ProductManager()
@@ -50,7 +70,20 @@ const env = async () => {
     // products.addProducts("product2", "kakak", 1500, "imagen2.jpg", 15, 45457)
     // products.addProducts("product3", "kakak", 1500, "imagen2.jpg", 15, 45458)
     // products.addProducts("product3", "kakak", 1500, "imagen2.jpg", 15, 45459)
-    console.log(await products.getProductByCode(45458))
+    // products.deleteProdcutcById(2)
+    // console.log(await products.getProductByCode(45458))
+    // products.deleteProdcutcByCode(45456)
+    products.updateProduct(
+        {
+            title: 'product3',
+            description: 'kakak',
+            price: 2500,
+            thumbnail: 'imagen2.jpg',
+            stock: 15,
+            code: 45459,
+            id: 4
+          }
+    )
 }
 
 env()
